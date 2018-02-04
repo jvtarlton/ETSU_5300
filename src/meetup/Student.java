5ecf60a46cbda26ad2f688a027f54f01ed6daf01
jvtarlton <jvtarlton@gmail.com>
2018-02-04 17:54:05 -0500

    toString method updated

--- src/meetup/Student.java
+++ src/meetup/Student.java
@@ -3,29 +3,36 @@ package meetup;
 import java.util.*;
 
 /**
- *
+ * This class represents the Student subclass user type
  * @author Jim
  */
 public class Student extends User {
     
+    // protected members
     protected boolean flagged;
     protected boolean ban;
-    protected ArrayList<Friend> friends;
-    protected ArrayList<Schedule> schedule;
-    protected ArrayList<Suggestion> suggestions;
+    protected List<Friend> friends;
+    protected List<Schedule> schedule;
+    protected List<Suggestion> suggestions;
     
-    public Student(int i, String n, boolean f, boolean b, ArrayList<Friend> fs, ArrayList<Schedule> sh) {
+    // constructor
+    public Student(
+            int i, String n, 
+            boolean f, boolean b, 
+            List<Friend> fs, 
+            List<Schedule> sh
+    ) {
         super(i, n);
         this.ban = b;
         this.friends = fs;
         this.schedule = sh;
     }
     
-    public ArrayList<Schedule> getSchedule() {
+    public List<Schedule> getSchedule() {
         return schedule;
     }
     
-    public ArrayList<Suggestion> getSuggestions() {
+    public List<Suggestion> getSuggestions() {
         return suggestions;
     }
 
@@ -38,4 +45,24 @@ public class Student extends User {
         // find common schedules
         // append to suggestions collection
     }
+
+    @Override
+    public String toString() {
+        String output =  
+            "\n------------------" +
+            "\nAccount Type: Student" +
+            "\nID: " + super.id + 
+            "\nName: " + super.name + 
+            "\nFriend count: " + this.friends.size() +
+            "\nSchedule is as follows: \n";
+            for(int i = 0; i < this.schedule.size(); i++) {
+                output += "\t" +
+                    this.schedule.get(i).name + " on " +
+                    this.schedule.get(i).time + " at " +
+                    this.schedule.get(i).location + "\n"; 
+            }
+        return output;
+    }
+    
+    
 }

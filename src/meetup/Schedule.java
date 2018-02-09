f82e90564b7679fa04450d112e0bfc5419ff4746
jvtarlton <jvtarlton@gmail.com>
2018-02-09 16:21:54 -0500

    main method commit

--- src/meetup/Schedule.java
+++ src/meetup/Schedule.java
@@ -2,21 +2,35 @@
 package meetup;
 
 /**
- *
+ * This class is a Student aggregate class
  * @author Jim
  */
 public class Schedule {
     
+    // protected members
     protected int id;
     protected String name;
     protected String time;
     protected String location;
 
+    // constructor
     public Schedule(int i, String n, String t, String l) {
         this.id = i;
         this.name = n;
         this.time = t;
         this.location = l;
     }
-
+    
+    // factory method
+    static Schedule make(int i, String n, String t, String l) {
+        return new Schedule(i, n, t, l); 
+    }
+    
+    // toString to print schedule item
+    @Override
+    public String toString() {
+        return "\n\t" + name + " on " + time + " at " + location + "\n";
+    }
+    
+    
 }

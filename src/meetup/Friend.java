1be7aa0ae3c0a0330d0539c4d0828c1456ffde76
jvtarlton <jvtarlton@gmail.com>
2018-02-09 16:21:20 -0500

    main method commit

--- src/meetup/Friend.java
+++ src/meetup/Friend.java
@@ -2,20 +2,32 @@
 package meetup;
 
 /**
- *
+ * This class is a Student aggregate class
  * @author Jim
  */
 public class Friend {
     
+    // private members
     protected int id;
     protected boolean blocked;
     
+    // constructor
     public Friend(int i, boolean b) {
         this.id = i;
         this.blocked = b;
     }
-    public void toggleBlock() {
-        this.blocked = !this.blocked;
+    
+    // factory method
+    static Friend make(int i, boolean b) {
+        return new Friend(i, b); 
     }
+    
+    // functionality to let student block/unblock a friend from friends list
+    public void toggleBlock() {
+        this.blocked = ((this.blocked == true) ? false : true);
+    }
+    
+    
+    
     
 }

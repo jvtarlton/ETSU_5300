fec79aada7016b2d1331f631aa1b0f570162e3ee
jvtarlton <jvtarlton@gmail.com>
2018-02-09 16:23:47 -0500

    main method commit

--- src/meetup/Message.java
+++ src/meetup/Message.java
@@ -2,21 +2,28 @@
 package meetup;
 
 /**
- *
+ * This class is a Student aggregate class
  * @author Jim
  */
 public class Message {
     
-    int id;
-    int receiver_id;
-    String sent_time;
-    String body;
+    // protected members
+    protected int id;
+    protected int receiver_id;
+    protected String sent_time;
+    protected String body;
     
+    // constructor
     public Message(int i, int r, String st, String b) {
         this.id = i;
         this.receiver_id = r;
         this.sent_time = st;
         this.body = b;
+    }
+    
+    // factory method
+    static Message make(int i, int r, String st, String b) {
+        return new Message(i, r, st, b); 
     }
     
 }
